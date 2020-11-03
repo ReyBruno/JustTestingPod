29df0f84a1c58640405345cd99eea64c235ca411
Rey <lbruno@yopapp.me>
2020-11-03 18:19:32 -0300

    First commit

---
+++ JustTestingPodFramework/JustTestingPodFramework/JustTestingPodFramework.h
@@ -0,0 +1,18 @@
+//
+//  JustTestingPodFramework.h
+//  JustTestingPodFramework
+//
+//  Created by Rey on 11/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JustTestingPodFramework.
+FOUNDATION_EXPORT double JustTestingPodFrameworkVersionNumber;
+
+//! Project version string for JustTestingPodFramework.
+FOUNDATION_EXPORT const unsigned char JustTestingPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustTestingPodFramework/PublicHeader.h>
+
+
